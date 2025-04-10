5f874db8289a42f7956fa879faf7d909112f4a95
wwobert <110064776+wobertt@users.noreply.github.com>
2025-04-10 00:17:55 -0400

    Use long long in WeightedMatching

--- content/graph/WeightedMatching.h
+++ content/graph/WeightedMatching.h
@@ -14,23 +14,25 @@
  */
 #pragma once
 
-pair<int, vi> hungarian(const vector<vi> &a) {
+using vll = vector<ll>;
+pair<ll, vll> hungarian(const vector<vll> &a) {
 	if (a.empty()) return {0, {}};
 	int n = sz(a) + 1, m = sz(a[0]) + 1;
-	vi u(n), v(m), p(m), ans(n - 1);
+	vll u(n), v(m), p(m), ans(n-1);
 	rep(i,1,n) {
 		p[0] = i;
 		int j0 = 0; // add "dummy" worker 0
-		vi dist(m, INT_MAX), pre(m, -1);
-		vector<bool> done(m + 1);
+		vll dist(m, LLONG_MAX), pre(m, -1);
+		vector<bool> done(m+1);
 		do { // dijkstra
 			done[j0] = true;
-			int i0 = p[j0], j1, delta = INT_MAX;
+			int i0 = p[j0], j1;
+			ll delta = LLONG_MAX;
 			rep(j,1,m) if (!done[j]) {
-				auto cur = a[i0 - 1][j - 1] - u[i0] - v[j];
-				if (cur < dist[j]) dist[j] = cur, pre[j] = j0;
-				if (dist[j] < delta) delta = dist[j], j1 = j;
-			}
+					ll cur = a[i0-1][j-1] - u[i0] - v[j];
+					if (cur < dist[j]) dist[j] = cur, pre[j] = j0;
+					if (dist[j] < delta) delta = dist[j], j1 = j;
+				}
 			rep(j,0,m) {
 				if (done[j]) u[p[j]] += delta, v[j] -= delta;
 				else dist[j] -= delta;
@@ -42,6 +44,6 @@ pair<int, vi> hungarian(const vector<vi> &a) {
 			p[j0] = p[j1], j0 = j1;
 		}
 	}
-	rep(j,1,m) if (p[j]) ans[p[j] - 1] = j - 1;
-	return {-v[0], ans}; // min cost
+	rep(j,1,m) if (p[j]) ans[p[j] - 1] = j-1;
+	return {-v[0], ans}; // {min cost, match vector}
 }
