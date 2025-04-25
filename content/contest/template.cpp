bb936b617610fabc2ec022846bf74b01be1ad42c
wwobert <110064776+wobertt@users.noreply.github.com>
2025-04-25 12:46:32 -0400

    Remove dbg from template.cpp

--- content/contest/template.cpp
+++ content/contest/template.cpp
@@ -1,20 +1,11 @@
 #include <bits/stdc++.h>
 using namespace std;
-#ifdef LOCAL
-#define dbg(x) cerr << "(Line " << __LINE__ << ") " \
-    << #x << ": " << x << '\n'
-#else
-#define dbg(x) 0
-#endif
 #define rep(i, a, b) for(int i = a; i < (b); ++i)
-#define rin(i, a, b) rep(i, a, b+1)
-#define fox(i, n) rep(i, 0, n)
 #define all(x) begin(x), end(x)
 #define sz(x) (int)(x).size()
 typedef long long ll;
 typedef pair<int, int> pii;
 typedef vector<int> vi;
-template<class T> using V = vector<T>;
 int main() {
 	cin.tie(0)->sync_with_stdio(0);
 	cin.exceptions(cin.failbit);
