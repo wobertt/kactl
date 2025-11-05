92b0591dd541f939c2a7f5c72847f68bbe3f670b
wwobert <110064776+wobertt@users.noreply.github.com>
2025-11-05 17:27:03 -0500

    Allow arbitrary types in WeightedMatching

--- content/graph/WeightedMatching.h
+++ content/graph/WeightedMatching.h
@@ -14,36 +14,38 @@
  */
 #pragma once
 
-using vll = vector<ll>;
-pair<ll, vll> hungarian(const vector<vll> &a) {
-	if (a.empty()) return {0, {}};
-	int n = sz(a) + 1, m = sz(a[0]) + 1;
-	vll u(n), v(m), p(m), ans(n-1);
-	rep(i,1,n) {
-		p[0] = i;
-		int j0 = 0; // add "dummy" worker 0
-		vll dist(m, LLONG_MAX), pre(m, -1);
-		vector<bool> done(m+1);
-		do { // dijkstra
-			done[j0] = true;
-			int i0 = p[j0], j1;
-			ll delta = LLONG_MAX;
-			rep(j,1,m) if (!done[j]) {
-					ll cur = a[i0-1][j-1] - u[i0] - v[j];
-					if (cur < dist[j]) dist[j] = cur, pre[j] = j0;
-					if (dist[j] < delta) delta = dist[j], j1 = j;
-				}
-			rep(j,0,m) {
-				if (done[j]) u[p[j]] += delta, v[j] -= delta;
-				else dist[j] -= delta;
-			}
-			j0 = j1;
-		} while (p[j0]);
-		while (j0) { // update alternating path
-			int j1 = pre[j0];
-			p[j0] = p[j1], j0 = j1;
-		}
-	}
-	rep(j,1,m) if (p[j]) ans[p[j] - 1] = j-1;
-	return {-v[0], ans}; // {min cost, match vector}
+template<class T>
+pair<T, vector<T>> hungarian(const vector<vector<T>> &a) {
+  if (a.empty()) return {0, {}};
+  int n = sz(a) + 1, m = sz(a[0]) + 1;
+  vector<T> u(n), v(m), ans(n-1);
+  vi p(m);
+  rep(i,1,n) {
+    p[0] = i;
+    int j0 = 0; // add "dummy" worker 0
+    vector<T> dist(m, 1e18); // change MAX if needed
+    vi pre(m, -1);
+    vector<bool> done(m+1);
+    do { // dijkstra
+      done[j0] = true;
+      int i0 = p[j0], j1;
+      T delta = 1e18; // change MAX if needed
+      rep(j,1,m) if (!done[j]) {
+          T cur = a[i0-1][j-1] - u[i0] - v[j];
+          if (cur < dist[j]) dist[j] = cur, pre[j] = j0;
+          if (dist[j] < delta) delta = dist[j], j1 = j;
+        }
+      rep(j,0,m) {
+        if (done[j]) u[p[j]] += delta, v[j] -= delta;
+        else dist[j] -= delta;
+      }
+      j0 = j1;
+    } while (p[j0]);
+    while (j0) { // update alternating path
+      int j1 = pre[j0];
+      p[j0] = p[j1], j0 = j1;
+    }
+  }
+  rep(j,1,m) if (p[j]) ans[p[j] - 1] = j-1;
+  return {-v[0], ans}; // {min cost, match vector}
 }
