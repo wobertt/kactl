0cd42b68aa708775b34769623d83932f446125fc
wwobert <110064776+wobertt@users.noreply.github.com>
2026-02-14 12:45:33 -0500

    Update ModularArithmetic for define int long long

--- content/number-theory/ModularArithmetic.h
+++ content/number-theory/ModularArithmetic.h
@@ -11,7 +11,7 @@ template<int M> struct mint {
     int v;
     explicit operator int() const { return v; }
     mint():v(0) {}
-    mint(ll _v):v(int(_v%M)) { v += (v<0)*M; }
+    mint(ll _v):v((int)(_v%M)) { v += (v<0)*M; }
     mint& operator+=(mint o) {
         if ((v += o.v) >= M) v -= M;
         return *this; }
@@ -19,7 +19,7 @@ template<int M> struct mint {
         if ((v -= o.v) < 0) v += M;
         return *this; }
     mint& operator*=(mint o) {
-        v = int((ll)v*o.v%M); return *this; }
+        v = (int)((ll)v*o.v%M); return *this; }
     friend mint pow(mint a, ll p) { assert(p >= 0);
         return p==0?1:pow(a*a,p/2)*(p&1?a:1); }
     friend mint inv(mint a) { assert(a.v != 0); return pow(a,M-2); }
@@ -27,4 +27,4 @@ template<int M> struct mint {
     friend mint operator-(mint a, mint b) { return a -= b; }
     friend mint operator*(mint a, mint b) { return a *= b; }
 };
-using mi = mint<7>; // change to something else
+using mi = mint<0>; // change to something else
