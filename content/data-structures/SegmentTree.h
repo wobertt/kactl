f1493e49f46a31917d19226ccaaa04171ceb153e
wwobert <110064776+wobertt@users.noreply.github.com>
2026-02-14 12:45:57 -0500

    Update SegmentTree.h unit

--- content/data-structures/SegmentTree.h
+++ content/data-structures/SegmentTree.h
@@ -12,7 +12,7 @@
 
 struct Tree {
 	typedef int T;
-	static constexpr T unit = INT_MIN;
+	inline static const T unit = INT_MIN;
 	T f(T a, T b) { return max(a, b); } // (any associative fn)
 	vector<T> s; int n;
 	Tree(int n = 0, T def = unit) : s(2*n, def), n(n) {}
