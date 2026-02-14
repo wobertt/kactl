a8e04fdf0172316f1cd1afd802e50fefe205f68e
wwobert <110064776+wobertt@users.noreply.github.com>
2026-02-14 12:45:47 -0500

    Add jdurie LazySegmentTree

--- content/data-structures/LazySegmentTree.h
+++ content/data-structures/LazySegmentTree.h
@@ -1,66 +1,70 @@
 /**
- * Author: Simon Lindholm
- * Date: 2016-10-08
- * License: CC0
- * Source: me
- * Description: Segment tree with ability to add or set values of large intervals, and compute max of intervals.
- * Can be changed to other things.
- * Use with a bump allocator for better performance, and SmallPtr or implicit indices to save memory.
- * Time: O(\log N).
- * Usage: Node* tr = new Node(v, 0, sz(v));
- * Status: stress-tested a bit
+ * Author: Joseph Durie
+ * Date: 2020-10-11
+ * Description: Remember that idT and idU must be idempotent
+ * Time: O(\log N)
  */
 #pragma once
 
-#include "../various/BumpAllocator.h"
-
-const int inf = 1e9;
-struct Node {
-	Node *l = 0, *r = 0;
-	int lo, hi, mset = inf, madd = 0, val = -inf;
-	Node(int lo,int hi):lo(lo),hi(hi){} // Large interval of -inf
-	Node(vi& v, int lo, int hi) : lo(lo), hi(hi) {
-		if (lo + 1 < hi) {
-			int mid = lo + (hi - lo)/2;
-			l = new Node(v, lo, mid); r = new Node(v, mid, hi);
-			val = max(l->val, r->val);
-		}
-		else val = v[lo];
+struct lztree {
+	typedef ll T; T idT = {}; // node type
+	typedef ll U; U idU = {}; // update type
+	// combining segtree nodes a and b
+	T f(T a, T b) { return a + b; }
+	// applying updates f and g (f compose g)
+	U g(U f, U g) { return f + g; }
+	// applying update f to segtree node a
+	T h(U f, T a) { return a + f; }
+	int n;
+	vector<T> t;
+	vector<U> d;
+	lztree(int n) : n(n), t(2*n, idT), d(n, idU) {}
+	void calc(ll p) { t[p] = h(d[p], f(t[p * 2], t[p * 2 + 1])); }
+	void apply(ll p, U v) {
+		t[p] = h(v, t[p]);
+		if(p < n) d[p] = g(v, d[p]);
 	}
-	int query(int L, int R) {
-		if (R <= lo || hi <= L) return -inf;
-		if (L <= lo && hi <= R) return val;
-		push();
-		return max(l->query(L, R), r->query(L, R));
-	}
-	void set(int L, int R, int x) {
-		if (R <= lo || hi <= L) return;
-		if (L <= lo && hi <= R) mset = val = x, madd = 0;
-		else {
-			push(), l->set(L, R, x), r->set(L, R, x);
-			val = max(l->val, r->val);
-		}
-	}
-	void add(int L, int R, int x) {
-		if (R <= lo || hi <= L) return;
-		if (L <= lo && hi <= R) {
-			if (mset != inf) mset += x;
-			else madd += x;
-			val += x;
-		}
-		else {
-			push(), l->add(L, R, x), r->add(L, R, x);
-			val = max(l->val, r->val);
+	void push(ll p) {
+		p += n;
+		for(int s = 24; s > 0; s--) { // need tree height <= 24
+			ll i = p >> s;
+			if(d[i] != idU) {
+				apply(i * 2, d[i]);
+				apply(i * 2 + 1, d[i]);
+				d[i] = idU;
+			}
 		}
 	}
-	void push() {
-		if (!l) {
-			int mid = lo + (hi - lo)/2;
-			l = new Node(lo, mid); r = new Node(mid, hi);
+	void set(ll p, T v) { // point set arr[p] = v
+		push(p);
+		t[p += n] = v;
+		while(p > 1) calc(p /= 2);
+	}
+	T get(ll p) { // return arr[p]
+        push(p);
+        return t[p + n];
+    }
+	void update(ll l, ll r, U v) { // range update [l, r) using v
+		push(l), push(r - 1);
+		bool cl = false, cr = false;
+		for(l += n, r += n; l < r; l /= 2, r /= 2) {
+			if(cl) calc(l - 1);
+			if(cr) calc(r);
+			if(l & 1) apply(l++, v), cl = true;
+			if(r & 1) apply(--r, v), cr = true;
 		}
-		if (mset != inf)
-			l->set(lo,hi,mset), r->set(lo,hi,mset), mset = inf;
-		else if (madd)
-			l->add(lo,hi,madd), r->add(lo,hi,madd), madd = 0;
+		for(--l; r; l /= 2, r /= 2) {
+			if(cl) calc(l);
+			if(cr) calc(r);
+		}
+	}
+	T query(ll l, ll r) { // range query [l, r)
+		push(l), push(r - 1);
+		T resl = idT, resr = idT;
+		for(l += n, r += n; l < r; l /= 2, r /= 2) {
+			if(l & 1) resl = f(resl, t[l++]);
+			if(r & 1) resr = f(t[--r], resr);
+		}
+		return f(resl, resr);
 	}
 };
