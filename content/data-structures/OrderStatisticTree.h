ce51dfc81e4a715d08c38058d254b3e2901828ab
wwobert <110064776+wobertt@users.noreply.github.com>
2026-02-18 13:23:16 -0500

    Add descriptions to OrderStatisticTree.h

--- content/data-structures/OrderStatisticTree.h
+++ content/data-structures/OrderStatisticTree.h
@@ -21,8 +21,9 @@ void example() {
 	Tree<int> t, t2; t.insert(8);
 	auto it = t.insert(10).first;
 	assert(it == t.lower_bound(9));
-	assert(t.order_of_key(10) == 1);
+	// t.order_of_key(x) = number of elmts strictly less than x
 	assert(t.order_of_key(11) == 2);
+	// t.find_by_order(k) = kth element, counting from 0
 	assert(*t.find_by_order(0) == 8);
 	t.join(t2); // assuming T < T2 or T > T2, merge t2 into t
 }
