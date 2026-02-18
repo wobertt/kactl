f865d9b6e6e830759b11bcaae635e9602b0c48f8
wwobert <110064776+wobertt@users.noreply.github.com>
2026-02-17 19:15:04 -0500

    Update ModularArithmetic.h (stream operators, casts)

--- content/number-theory/ModularArithmetic.h
+++ content/number-theory/ModularArithmetic.h
@@ -11,7 +11,7 @@ template<int M> struct mint {
     int v;
     explicit operator int() const { return v; }
     mint():v(0) {}
-    mint(ll _v):v((int)(_v%M)) { v += (v<0)*M; }
+    mint(ll _v):v(_v%M) { v += (v<0)*M; }
     mint& operator+=(mint o) {
         if ((v += o.v) >= M) v -= M;
         return *this; }
@@ -19,12 +19,17 @@ template<int M> struct mint {
         if ((v -= o.v) < 0) v += M;
         return *this; }
     mint& operator*=(mint o) {
-        v = (int)((ll)v*o.v%M); return *this; }
+        v = (ll)v*o.v%M; return *this; }
     friend mint pow(mint a, ll p) { assert(p >= 0);
         return p==0?1:pow(a*a,p/2)*(p&1?a:1); }
-    friend mint inv(mint a) { assert(a.v != 0); return pow(a,M-2); }
+    friend mint inv(mint a) { assert(a.v != 0);
+        return pow(a,M-2); }
     friend mint operator+(mint a, mint b) { return a += b; }
     friend mint operator-(mint a, mint b) { return a -= b; }
     friend mint operator*(mint a, mint b) { return a *= b; }
+    friend istream& operator>>(istream& is, mint& a) { // &
+        return is >> a.v; }
+    friend ostream& operator<<(ostream& os, mint a) { // no &
+        return os << a.v; }
 };
 using mi = mint<0>; // change to something else
