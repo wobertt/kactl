708c534774357ce5e73fc70e0fd20eb5897e8dda
wwobert <110064776+wobertt@users.noreply.github.com>
2026-03-03 02:31:44 -0500

    Add cdiv, fdiv to template

--- content/contest/template.cpp
+++ content/contest/template.cpp
@@ -6,6 +6,8 @@ using namespace std;
 typedef long long ll;
 typedef pair<int, int> pii;
 typedef vector<int> vi;
+ll cdiv(ll a, ll b) { return a/b+((a^b)>0&&a%b); }
+ll fdiv(ll a, ll b) { return a/b-((a^b)<0&&a%b); }
 int main() {
 	cin.tie(0)->sync_with_stdio(0);
 	cin.exceptions(cin.failbit);
