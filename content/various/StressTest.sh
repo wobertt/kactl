53a61a854ae6d791616ecc220b4a7224c4bfa739
wwobert <110064776+wobertt@users.noreply.github.com>
2026-03-04 23:58:38 -0500

    Fix StressTest.sh bugs

--- content/various/StressTest.sh
+++ content/various/StressTest.sh
@@ -2,17 +2,17 @@
 # Example: bash stress.sh main main2 generator 1000
 
 # remove this and add -O2 if you want
-flags=-std=c++20 -g -fsanitize=address,undefined
+flags="-std=c++20 -g -fsanitize=address,undefined"
 
 g++ $flags $1.cpp -o $1
 g++ $flags $2.cpp -o $2
 g++ $flags $3.cpp -o $3
 
-for ((t = 1; t <= $4; t++)) do
+for ((t=1; t<=$4; t++)) do
   ./$3 > input
   ./$2 < input > outslow
   ./$1 < input > outwrong
-  if [[ "$?" -ne 0 ]] || ! (cmp -s "outWrong" "outSlow") then
+  if [[ "$?" -ne 0 ]] || ! (cmp -s "outwrong" "outslow") then
     echo error found
     echo input:
     cat input
