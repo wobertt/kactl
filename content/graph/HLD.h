fa091edab1e2d96136bf90eb1f9190909b8464fc
wwobert <110064776+wobertt@users.noreply.github.com>
2026-03-16 17:37:33 -0400

    Use lztree in HLD.h

--- content/graph/HLD.h
+++ content/graph/HLD.h
@@ -5,29 +5,30 @@
  * Source: https://codeforces.com/blog/entry/53170, https://github.com/bqi343/USACO/blob/master/Implementations/content/graphs%20(12)/Trees%20(10)/HLD%20(10.3).h
  * Description: Decomposes a tree into vertex disjoint heavy paths and light
  * edges such that the path from any leaf to the root contains at most log(n)
- * light edges. Code does additive modifications and max queries, but can
- * support commutative segtree modifications/queries on paths and subtrees.
- * Takes as input the full adjacency list. VALS\_EDGES being true means that
- * values are stored in the edges, as opposed to the nodes. All values
- * initialized to the segtree default. Root must be 0.
+ * light edges. Takes as input the adjacency list (comment out (*) in dfsSz if no parent edges),
+ * a list of initial values $v$, and the root. VALS\_EDGES being true means
+ * that values are stored in the edges, as opposed to the nodes.
  * Time: O((\log N)^2)
- * Status: stress-tested against old HLD
+ * Status: tested on https://cses.fi/problemset/result/16580118/
  */
 #pragma once
 
 #include "../data-structures/LazySegmentTree.h"
 
 template <bool VALS_EDGES> struct HLD {
-	int N, tim = 0;
-	vector<vi> adj;
-	vi par, siz, rt, pos;
-	Node *tree;
-	HLD(vector<vi> adj_)
-		: N(sz(adj_)), adj(adj_), par(N, -1), siz(N, 1),
-		  rt(N),pos(N),tree(new Node(0, N)){ dfsSz(0); dfsHld(0); }
+    int N, tim = 0;
+    vector<vi> adj;
+    vi par, siz, rt, pos;
+    lztree tree;
+    HLD(vector<vi> &adj, vector<T> v, int root)
+        : N(sz(adj)), adj(adj), par(N, -1), siz(N, 1),
+          rt(N),pos(N),tree(N) {
+        dfsSz(root); dfsHld(root);
+        rep(i,root,sz(v)) tree.set(pos[i], v[i]); // init vals
+    }
 	void dfsSz(int v) {
 		for (int& u : adj[v]) {
-			adj[u].erase(find(all(adj[u]), v));
+			adj[u].erase(find(all(adj[u]), v)); // (*)
 			par[u] = v;
 			dfsSz(u);
 			siz[v] += siz[u];
@@ -49,17 +50,20 @@ template <bool VALS_EDGES> struct HLD {
 		}
 		op(pos[u] + VALS_EDGES, pos[v] + 1);
 	}
-	void modifyPath(int u, int v, int val) {
-		process(u, v, [&](int l, int r) { tree->add(l, r, val); });
+    T get(int u) { // point set is similar
+        return tree.get(pos[u]);
+    }
+	void modifyPath(int u, int v, U val) {
+		process(u, v, [&](int l, int r) { tree.update(l, r, val); });
 	}
-	int queryPath(int u, int v) { // Modify depending on problem
-		int res = -1e9;
-		process(u, v, [&](int l, int r) {
-				res = max(res, tree->query(l, r));
-		});
-		return res;
-	}
-	int querySubtree(int v) { // modifySubtree is similar
-		return tree->query(pos[v] + VALS_EDGES, pos[v] + siz[v]);
-	}
+    T queryPath(int u, int v) { // Modify depending on problem
+        T res = idT; // unit
+        process(u, v, [&](int l, int r) { // res = op(res, query)
+                res = f(res, tree.query(l, r));
+        });
+        return res;
+    }
+    T querySubtree(int v) { // modifySubtree is similar
+        return tree.query(pos[v] + VALS_EDGES, pos[v] + siz[v]);
+    }
 };
