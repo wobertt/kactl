d061d3d3666e93334ad933dfab37ebb354aa5b3e
wwobert <110064776+wobertt@users.noreply.github.com>
2026-03-16 11:47:53 -0400

    Refactor MoQueries.h with structured bindings

--- content/data-structures/MoQueries.h
+++ content/data-structures/MoQueries.h
@@ -20,15 +20,14 @@ vi mo(vector<pii> Q) {
 #define K(x) pii(x.first/blk, x.second ^ -(x.first/blk & 1))
 	iota(all(s), 0);
 	sort(all(s), [&](int s, int t){ return K(Q[s]) < K(Q[t]); });
-	for (int qi : s) {
-		pii q = Q[qi];
-		while (L > q.first) add(--L, 0);
-		while (R < q.second) add(R++, 1);
-		while (L < q.first) del(L++, 0);
-		while (R > q.second) del(--R, 1);
-		res[qi] = calc();
-	}
-	return res;
+    for (int qi : s) {
+        auto [x, y] = Q[qi];
+        while (L > x) add(--L, 0);
+        while (R < y) add(R++, 1);
+        while (L < x) del(L++, 0);
+        while (R > y) del(--R, 1);
+        res[qi] = calc();
+    }
 	return res; // NOTE: Q must have inclusive-exclusive intervals.
 }
 
