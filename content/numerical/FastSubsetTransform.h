1ccc87e0508b8edd121691efff13bfda1f6284c2
wwobert <110064776+wobertt@users.noreply.github.com>
2026-04-07 22:57:41 -0400

    Use ll instead of int in FastSubsetTransform.h

--- content/numerical/FastSubsetTransform.h
+++ content/numerical/FastSubsetTransform.h
@@ -11,33 +11,34 @@
  */
 #pragma once
 
-void FST(vi& a, bool inv) {
-	for (int n = sz(a), step = 1; step < n; step *= 2) {
-		for (int i = 0; i < n; i += 2 * step) rep(j,i,i+step) {
-			int &u = a[j], &v = a[j + step]; tie(u, v) =
-				// inv ? pii(v - u, u) : pii(v, u + v); // AND /// include-line
-				// inv ? pii(v, u - v) : pii(u + v, u); // OR /// include-line
-				pii(u + v, u - v);                   // XOR
-		}
-	}
-	if (inv) for (int& x : a) x /= sz(a); // XOR
+using vl = vector<ll>;
+void FST(vl& a, bool inv) {
+    for (int n = sz(a), step = 1; step < n; step *= 2) {
+        for (int i = 0; i < n; i += 2 * step) rep(j,i,i+step) {
+            ll &u = a[j], &v = a[j + step]; tie(u, v) =
+                // inv ? pair(v - u, u) : pair(v, u + v); // AND /// include-line
+                // inv ? pair(v, u - v) : pair(u + v, u); // OR /// include-line
+                pair(u + v, u - v);                   // XOR
+        }
+    }
+    if (inv) for (ll& x : a) x /= sz(a); // XOR
 }
-vi conv(vi a, vi b) {
-	FST(a, 0); FST(b, 0);
-	rep(i,0,sz(a)) a[i] *= b[i];
-	FST(a, 1); return a;
+vl conv(vl a, vl b) {
+    FST(a, 0); FST(b, 0);
+    rep(i,0,sz(a)) a[i] *= b[i];
+    FST(a, 1); return a;
 }
 int psgn(int x) { return __builtin_popcount(x)&1 ? -1 : 1; }
 // a[i] += val (for transformed a)
-void point_add(vi &a, int i, int val) {
+void point_add(vl &a, int i, ll val) {
     rep(j,0,sz(a))
         // a[j] += val * ((i|j) == sz(a)-1); // AND /// include-line
         // a[j] += val * ((i&j) == 0); // OR /// include-line
         a[j] += val * psgn(i&j); // XOR
 }
 // return a[i] (for transformed a)
-int point_eval(const vi &a, int i) {
-    int res = 0;
+ll point_eval(const vl &a, int i) {
+    ll res = 0;
     rep(j,0,sz(a))
         // res += a[j] * ((i&j) == 0) * psgn(sz(a)-1-(i|j)); // AND /// include-line
         // res += a[j] * ((i|j) == sz(a)-1) * psgn(i&j); // OR /// include-line
